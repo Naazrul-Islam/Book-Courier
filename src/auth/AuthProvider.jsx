ce58b0da0cff31fd3340fa4bf1230b0a9a03ea0f
Naazrul-Islam <hmnazrulislam06@gmail.com>
2025-12-05 23:00:06 -0600

    router set up added

---
+++ src/auth/AuthProvider.jsx
@@ -0,0 +1,78 @@
+import React, {  useEffect, useState } from "react";
+
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../firebaseConfig";
+
+
+const provider = new GoogleAuthProvider();
+
+
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // console.log(loading, user);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  const logout = () => {
+    setUser(null);
+    return signOut(auth);
+  };
+  const googleSignIn = () => {
+    return signInWithPopup(auth, provider);
+  };
+  const ForgetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authData = {
+    user,
+    setUser,
+    createUser,
+    logout,
+    signIn,
+    loading,
+    setLoading,
+    updateUser,
+    googleSignIn,
+    ForgetPassword
+  };
+  return <AuthContext value={authData}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOuZz7wo-xjO_3lA2OTx5zXYLR-VSJp2M",
+  authDomain: "bookcourier-3f07a.firebaseapp.com",
+  projectId: "bookcourier-3f07a",
+  storageBucket: "bookcourier-3f07a.firebasestorage.app",
+  messagingSenderId: "1068867463414",
+  appId: "1:1068867463414:web:1d5e8c5d6c2e89105524d7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
